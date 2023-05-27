7282fca4c86725a71bca1bcb522888fac6f067bf
rayaaldyen <100760188+rayaaldyen@users.noreply.github.com>
2023-05-27 20:56:26 +0700

    Menambahkan fitur weather view berdasarkan lokasi perangkat dan tema weather view akan berubah sesuai dengan kondisi cuaca

--- app/src/main/java/com/example/mybottomnav/ui/history/HistoryFragment.kt
+++ app/src/main/java/com/example/mybottomnav/ui/history/HistoryFragment.kt
@@ -1,5 +1,7 @@
 package com.example.mybottomnav.ui.history
 
+import android.annotation.SuppressLint
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -7,33 +9,61 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.example.mybottomnav.R
+import com.example.mybottomnav.adapter.PlantAdapter
+import com.example.mybottomnav.data.Plant
 import com.example.mybottomnav.databinding.FragmentHistoryBinding
+import com.example.mybottomnav.ui.Detail.DetailActivity
 
 class HistoryFragment : Fragment() {
 
     private var _binding: FragmentHistoryBinding? = null
-
-    // This property is only valid between onCreateView and
-    // onDestroyView.
     private val binding get() = _binding!!
+    private var list: ArrayList<Plant> = arrayListOf()
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val historyViewModel =
-            ViewModelProvider(this).get(HistoryViewModel::class.java)
-
         _binding = FragmentHistoryBinding.inflate(inflater, container, false)
         val root: View = binding.root
-
-        val recycleView: RecyclerView = binding.rvPlantsHistory
-//        historyViewModel.text.observe(viewLifecycleOwner) {
-//            textView.text = it
-//        }
+        list.addAll(getPlantData())
+        showRecyclerList()
         return root
+    }
+    private fun showRecyclerList(){
+        binding.rvPlantsHistory.layoutManager = LinearLayoutManager(requireContext())
+        val plantAdapter = PlantAdapter(list)
+        binding.rvPlantsHistory.adapter = plantAdapter
+
+        plantAdapter.setOnItemClickCallback(object : PlantAdapter.OnItemClickCallback{
+            override fun onItemClicked(data: Plant) {
+                showSelectPlant(data)
+            }
+        })
+    }
+
+    private fun showSelectPlant(plant: Plant){
+        val plant = Plant(plant.photoUrl, plant.nama, plant.detail)
+        val detailActivity = Intent(activity, DetailActivity::class.java)
+        detailActivity.putExtra(DetailActivity.EXTRA_PLANT, plant)
+        startActivity(detailActivity)
+    }
+
+    @SuppressLint("Recycle")
+    private fun getPlantData(): ArrayList<Plant>{
+        val dataNama = resources.getStringArray(R.array.data_result)
+        val dataDesk = resources.getStringArray(R.array.data_deskripsi)
+        val dataPhoto = resources.obtainTypedArray(R.array.data_result_photo)
+        val listPlant = ArrayList<Plant>()
+        for(i in dataNama.indices){
+            val plant = Plant( dataPhoto.getResourceId(i, -1), dataNama[i], dataDesk[i])
+            listPlant.add(plant)
+        }
+        return listPlant
     }
 
     override fun onDestroyView() {
--- app/src/main/java/com/example/mybottomnav/ui/welcome/SplashActivity.kt
+++ app/src/main/java/com/example/mybottomnav/ui/welcome/SplashActivity.kt
@@ -1,44 +1,24 @@
 package com.example.mybottomnav.ui.welcome
 
+import android.animation.AnimatorSet
+import android.animation.ObjectAnimator
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import android.view.View
 import com.example.mybottomnav.MainActivity
 import com.example.mybottomnav.R
-import com.example.mybottomnav.databinding.ActivitySplashBinding
-import com.example.mybottomnav.ui.login.LoginActivity
 
 class SplashActivity : AppCompatActivity() {
-    private lateinit var binding: ActivitySplashBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
         supportActionBar?.hide()
-//        playAnimation()
         Handler(Looper.getMainLooper()).postDelayed({
             startActivity(Intent(this@SplashActivity, MainActivity::class.java))
             finish()
         }, 3000)
     }
-
-//    private fun playAnimation() {
-//        val welcomeImage =
-//            ObjectAnimator.ofFloat(binding.welcomeImage, View.ALPHA, 1f).setDuration(500)
-//        val name = ObjectAnimator.ofFloat(binding.appName, View.ALPHA, 1f).setDuration(500)
-//        val appDesc =
-//            ObjectAnimator.ofFloat(binding.appDesc, View.ALPHA, 1f).setDuration(500)
-//
-//
-//        AnimatorSet().apply {
-//            playSequentially(
-//                welcomeImage,
-//                name,
-//                appDesc,
-//            )
-//            startDelay = 500
-//            start()
-//        }
-//    }
 }
