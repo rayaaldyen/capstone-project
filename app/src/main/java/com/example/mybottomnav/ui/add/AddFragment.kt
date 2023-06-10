9db9a456fd33e36b6833170284abe8254361dcfb
Elang Sotya <elangbeatles@gmail.com>
2023-06-11 00:31:44 +0700

    + revisi tampilan add fragment

--- app/src/main/java/com/example/mybottomnav/ui/add/AddFragment.kt
+++ app/src/main/java/com/example/mybottomnav/ui/add/AddFragment.kt
@@ -2,14 +2,19 @@ package com.example.mybottomnav.ui.add
 
 import android.animation.AnimatorSet
 import android.animation.ObjectAnimator
+import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
+import android.text.TextUtils.replace
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.example.mybottomnav.R
 import com.example.mybottomnav.databinding.FragmentAddBinding
 import com.example.mybottomnav.ui.result.ResultActivity
+import com.example.mybottomnav.ui.result.ResultFragment
 
 class AddFragment : Fragment() {
 
@@ -24,11 +29,11 @@ class AddFragment : Fragment() {
 
         _binding = FragmentAddBinding.inflate(inflater, container, false)
         val root: View = binding.root
-
         playAnimation()
-        binding.predictButton.setOnClickListener {
+        binding.constraintLayout1.setOnClickListener {
             startActivity(Intent(activity, ResultActivity::class.java))
         }
+
         return root
 
 
@@ -39,20 +44,22 @@ class AddFragment : Fragment() {
         _binding = null
     }
 
-    private fun playAnimation(){
-        ObjectAnimator.ofFloat(binding.imageView3, View.ALPHA, 1f).setDuration(1000).start()
-        ObjectAnimator.ofFloat(binding.predictTitle, View.ALPHA, 1f).setDuration(1000).start()
-        ObjectAnimator.ofFloat(binding.backgroundLayout, View.TRANSLATION_Y, 400f, -50f).setDuration(1500).start()
-        val etPH = ObjectAnimator.ofFloat(binding.phEditText, View.ALPHA, 1f).setDuration(300)
-        val etRain = ObjectAnimator.ofFloat(binding.rainEditText, View.ALPHA, 1f).setDuration(300)
-        val etCity = ObjectAnimator.ofFloat(binding.citySelection, View.ALPHA, 1f).setDuration(300)
-        val etSuhu = ObjectAnimator.ofFloat(binding.temperatureEditText, View.ALPHA, 1f).setDuration(300)
-        val btnPredict = ObjectAnimator.ofFloat(binding.predictButton, View.ALPHA, 1f).setDuration(300)
-        val titleInput = ObjectAnimator.ofFloat(binding.textDashboard, View.ALPHA, 1f).setDuration(600)
+    @SuppressLint("Recycle")
+    private fun playAnimation() {
+            ObjectAnimator.ofFloat(binding.imageView, View.ALPHA, 1f).setDuration(1000).start()
+            ObjectAnimator.ofFloat(binding.predictTitle, View.ALPHA, 1f).setDuration(1000).start()
+            ObjectAnimator.ofFloat(binding.textDashboard, View.ALPHA, 1f).setDuration(1000).start()
 
+        ObjectAnimator.ofFloat(binding.backgroundLayout, View.TRANSLATION_Y, 400f, -50f)
+            .setDuration(1000).start()
+        val recom =
+            ObjectAnimator.ofFloat(binding.constraintLayout1, View.ALPHA, 1f).setDuration(300)
+        val predict =
+            ObjectAnimator.ofFloat(binding.constraintLayout2, View.ALPHA, 1f).setDuration(300)
         AnimatorSet().apply {
-            playSequentially(etPH, etRain, etCity, etSuhu, btnPredict, titleInput)
+            playSequentially(recom, predict)
             start()
         }
+
     }
 }
